aa7159ceefb4163c6c73dfd52425157a52e95b81
Strelkova Olga <strelod@yandex.ru>
2022-07-04 15:50:50 +0300

    eslint debug

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log('Hey');
+  console.log(`The app is listened on port ${PORT}`);
 });
