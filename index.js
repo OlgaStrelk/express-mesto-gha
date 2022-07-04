c55183193661fbbe180794eb0868444c2b418452
Strelkova Olga <strelod@yandex.ru>
2022-07-04 16:10:38 +0300

    connect with database

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mydb');
+mongoose.connect('mongodb://localhost:27017/mestodb');
 const PORT = 3000;
 
 app.listen(PORT, () => {
