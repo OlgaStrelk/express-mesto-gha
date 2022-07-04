2b85597d0cc372d513c2893ac9842f043201ea23
Strelkova Olga <strelod@yandex.ru>
2022-07-04 16:09:48 +0300

    connect with database

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/mydb');
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`The app is listened on port ${PORT}`);
+  console.log(`App listening on port ${PORT}`);
 });
