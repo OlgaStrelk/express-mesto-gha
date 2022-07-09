9e7b48aa77f1fbd41bfadbc138b27f79c8e1c5ff
Strelkova Olga <strelod@yandex.ru>
2022-07-09 23:24:54 +0300

    add cards' controllers

--- controllers/card.js
+++ controllers/card.js
@@ -1,3 +1,22 @@
+const Card = require('../models/card');
+
 module.exports.createCard = (req, res) => {
-  res.send(req.user._id);
+  // res.send(req.user._id);
+  const { name, link } = req.body;
+
+  Card.create({ name, link })
+    .then((card) => res.send({ data: card }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+module.exports.deleteCardById = (req, res) => {
+  Card.findById(req.params.id)
+    .then((card) => res.send({ data: card }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+module.exports.getCards = (req, res) => {
+  Card.find({})
+    .then((card) => res.send({ data: card }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
