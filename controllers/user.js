2126ac28a2cfbb7350c025ea3a5a58c24fc0a7c6
Strelkova Olga <strelod@yandex.ru>
2022-07-10 23:07:10 +0300

    validation for user's routes

--- controllers/user.js
+++ controllers/user.js
@@ -35,7 +35,17 @@ module.exports.updateProfile = (req, res) => {
 
   User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
     .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка на стороне сервера на стороне сервера' }));
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(404).send({ message: 'Пользователь с таким id не найден' });
+        return;
+      }
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные пользователя' });
+        return;
+      }
+      res.status(500).send({ message: 'Произошла ошибка на стороне сервера' });
+    });
 };
 
 module.exports.updateAvatar = (req, res) => {
@@ -43,5 +53,16 @@ module.exports.updateAvatar = (req, res) => {
 
   User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
     .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка на стороне сервера на стороне сервера' }));
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(404).send({ message: 'Пользователь с таким id не найден' });
+        return;
+      }
+      if (err.name === 'ValidationError') {
+        res.status(400)
+          .send({ message: 'Переданы некорректные данные при обновлении аватара' });
+        return;
+      }
+      res.status(500).send({ message: 'Произошла ошибка на стороне сервера' });
+    });
 };
