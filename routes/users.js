e2d0d037a7fb02f834a117f4475b3899cf8658ec
Strelkova Olga <strelod@yandex.ru>
2022-07-10 15:53:15 +0300

    add user routes

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,20 @@
 const router = require('express').Router();
-const { createUser, getUserById, getUsers } = require('../controllers/user');
+const {
+  createUser,
+  getUserById,
+  getUsers,
+  updateProfile,
+  updateAvatar,
+} = require('../controllers/user');
 
 router.get('/users', getUsers);
 
 router.get('/users/:id', getUserById);
 
 router.post('/users', createUser);
+
+router.patch('/users/me', updateProfile);
+
+router.patch('/users/me/avatar', updateAvatar);
 
 module.exports = router;
