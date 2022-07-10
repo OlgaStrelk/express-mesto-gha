f0db3c581d20d6592549243d899a97c114525a70
Strelkova Olga <strelod@yandex.ru>
2022-07-10 17:12:18 +0300

    add cards routes

--- routes/cards.js
+++ routes/cards.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
-const { createCard } = require('../controllers/card');
+const {
+  createCard, getCards, deleteCardById, likeCard, dislikeCard,
+} = require('../controllers/card');
 
 router.post('/cards', createCard);
+router.get('/cards', getCards);
+router.delete('cards/:cardId', deleteCardById);
+router.put('cards/:cardId/likes', likeCard);
+router.delete('cards/:cardId/likes', dislikeCard);
 
 module.exports = router;
