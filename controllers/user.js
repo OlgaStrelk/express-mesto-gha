6c131c9732e86934853065dc7c943e4df6ef2a8b
Strelkova Olga <strelod@yandex.ru>
2022-07-13 00:05:35 +0300

    update create user controller

--- controllers/user.js
+++ controllers/user.js
@@ -3,9 +3,9 @@
 const User = require('../models/user');
 
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
+  const { name, about, avatar, email, password } = req.body;
 
-  User.create({ name, about, avatar })
+  User.create({ name, about, avatar, email, password })
     .then((user) => res.send({ data: user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
