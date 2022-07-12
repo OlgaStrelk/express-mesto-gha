3ed380e2c5d6038be1e2231d14bbdfc5a9fcafc5
Strelkova Olga <strelod@yandex.ru>
2022-07-12 19:38:50 +0300

    user model update defaults

--- models/user.js
+++ models/user.js
@@ -3,19 +3,30 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
+    default: 'Симон де Бовуар',
   },
 
   about: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
+    default: 'мыслительница',
   },
 
   avatar: {
+    type: String,
+    default: 'https://www.novochag.ru/upload/img_cache/e08/e08c959ebbe5335ba627a87fb73ce72a_ce_1042x720x56x0_cropped_666x444.jpg',
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  password: {
     type: String,
     required: true,
   },
