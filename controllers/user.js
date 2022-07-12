397bde0dd2675db8878faa7162a94f7ec9b692b3
Strelkova Olga <strelod@yandex.ru>
2022-07-13 00:22:25 +0300

    feat hash to createuser

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,15 @@
-// const bcrypt = require('bcryptjs'); // модуль для хэширования пароля
+const bcrypt = require('bcryptjs'); // модуль для хэширования пароля
 // const jwt = require('jsonwebtoken'); модуль для создания токена
 const User = require('../models/user');
 
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar, email, password } = req.body;
-
-  User.create({ name, about, avatar, email, password })
+  const {
+    name, about, avatar, email, password,
+  } = req.body;
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      name, about, avatar, email, password: hash,
+    }))
     .then((user) => res.send({ data: user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
