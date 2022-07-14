9c91af9030a87778a60ce05432f39c30db1ed810
Olga Strelkova <86570826+OlgaStrelk@users.noreply.github.com>
2022-07-14 23:57:27 +0300

    Update cards.js

--- routes/cards.js
+++ routes/cards.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const {
   createCard, getCards, deleteCardById, likeCard, dislikeCard,
 } = require('../controllers/cards');
-
+ 
 router.post('/cards', createCard);
 router.get('/cards', getCards);
 router.delete('/cards/:cardId', deleteCardById);
