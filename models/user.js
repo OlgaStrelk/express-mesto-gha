0c4975dcfb10f84e8fd50be034e839043b93cdd5
Strelkova Olga <strelod@yandex.ru>
2022-08-02 12:45:14 +0300

    fix findUserCredentials err

--- models/user.js
+++ models/user.js
@@ -1,12 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
-
-const errorWrongEmailOrPassword = () => {
-  const error = new Error('Неправильные почта или пароль');
-  error.statusCode = 403;
-  return error;
-};
+const UnauthorizedError = require('../utils/errors/UnauthorizedError');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -25,7 +20,8 @@ const userSchema = new mongoose.Schema({
 
   avatar: {
     type: String,
-    default: 'https://www.novochag.ru/upload/img_cache/e08/e08c959ebbe5335ba627a87fb73ce72a_ce_1042x720x56x0_cropped_666x444.jpg',
+    default:
+      'https://www.novochag.ru/upload/img_cache/e08/e08c959ebbe5335ba627a87fb73ce72a_ce_1042x720x56x0_cropped_666x444.jpg',
   },
 
   email: {
@@ -54,19 +50,19 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email }).select('+password')
+  return this.findOne({ email })
+    .select('+password')
     .then((user) => {
       if (!user) {
-        errorWrongEmailOrPassword();
+        return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
       }
 
-      return bcrypt.compare(password, user.password)
-        .then((matched) => {
-          if (!matched) {
-            errorWrongEmailOrPassword();
-          }
-          return user;
-        });
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
+        }
+        return user;
+      });
     });
 };
 
